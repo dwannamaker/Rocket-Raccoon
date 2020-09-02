929a98e9432cc36e909bae94ac7cb432209c9ab1
dwannamaker <wannamaker.dylan@gmail.com>
2020-09-01 20:37:48 -0700

    raccoon square can now move accross the y axis and stays within the container

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 console.log("= = = Prepare to Launch! = = =");
 
-const racDefPos = 240;
-
 const upArrow = 38; 
 const downArrow = 40;
 
-let moveUp = 15;
-let moveDown = 15;
-
+let position = 240;
 
 // read about jquery Position
 // need a default position (like in css with top) established in jquery
@@ -17,24 +13,27 @@ function moveRaccoon() {
     $("body").on("keydown", function(event) {
         
         if(event.originalEvent.keyCode === upArrow) {
-            console.log("Hi! I am the UP arrow!")
-            $(".raccoon").css("top", `${moveUp-=15}px`)
+            console.log("Hello! I am the UP arrow!")
+            if(position > 0) {
+                 $(".raccoon").css("top", `${position-=20}px`)
+            }
+            
         }
-
 
         if(event.originalEvent.keyCode === downArrow) {
-            console.log("Hi! I am the DOWN arrow!")
-            $(".raccoon").css("top", `${moveDown+=15}px`)
-
+            console.log("Hello! I am the DOWN arrow!")
+            if(position < 300) {
+                $(".raccoon").css("top", `${position+=20}px`)
+            }
         }
-    
-    
     })
-
-
-
 }
 
 $(document).ready(moveRaccoon);
 
 
+// create function to create an element, put it on page, and move it - from right side to left side of page 
+
+
+
+// obstacle 12and3 and randomly generate
