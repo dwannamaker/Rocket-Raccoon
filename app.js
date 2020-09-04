9a011acdc5d9fec8af449ead6532caab53a3aa22
dwannamaker <wannamaker.dylan@gmail.com>
2020-09-04 03:42:19 -0700

    progress commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const upArrow = 38;
 const downArrow = 40;
 
 let position = 240;
-const $raccoon = $("#raccoon").first()
+const $raccoon = $(".raccoon").first()
     console.log($raccoon);
 
 
@@ -17,19 +17,19 @@ function moveRaccoon() {
     $("body").on("keydown", function(event) {
 
         if(event.originalEvent.keyCode === upArrow) {
-            console.log("Hello! I am the UP arrow!")
-            console.log($raccoon.position());
+        //    console.log("Hello! I am the UP arrow!")
+        //    console.log($raccoon.position());
             if(position > 0) {
-                 $("#raccoon").css("top", `${position-=20}px`)
+                 $(".raccoon").css("top", `${position-=20}px`)
             }
             
         }
 
         if(event.originalEvent.keyCode === downArrow) {
-            console.log("Hello! I am the DOWN arrow!")
-            console.log($raccoon.position());
+        //    console.log("Hello! I am the DOWN arrow!")
+        //    console.log($raccoon.position());
             if(position < 300) {
-                $("#raccoon").css("top", `${position+=20}px`)
+                $(".raccoon").css("top", `${position+=20}px`)
             }
         }
     })
@@ -53,7 +53,7 @@ function checkingCollision() {
     const racPosition = $raccoon.position();
     const $car = $(".car").first();
 //        console.log($car);
-   // console.log(racPosition, $car);
+//        console.log(racPosition, $car);
     let carTop = $car.position().top;
     let carLeft = $car.position().left;
     let carBottom = carTop + $car.height();
@@ -65,12 +65,18 @@ function checkingCollision() {
     if (racBottom >= carTop && racRight >= carLeft) {
         return true;
     }
-    return false;
+        return false;
 }
+
 setInterval(() => {
     console.log("checking for collisions")
      if(checkingCollision()){
-        console.log("CRASH");
+         console.log("CRASH");
+    //    $(".container").freeze($(".container"));    
+    alert ("CRASH - GAME OVER")
+    document.location.reload();
+    clearInterval(interval);
+
     }
 }, 100)
 
@@ -80,8 +86,13 @@ setInterval(() => {
 // CRASH is being run whenever the obstacle crosses above raccoon and hits the Y axis, bottem clear for now. 
 // => need to fix CRASH 
 // => need to establish prompt to freeze game once CRASH occurs 
+        // object.freeze()
 
 
+// function gameOver() {
+
+//     if (checkingCollision(true));
+// }
 
 
 
@@ -89,8 +100,8 @@ setInterval(() => {
 
 // function to run and when 
 
-    //     if(racPosition.left === $car.left) {
-//         console.log(racPosition.left, $car.left);
+//     if(racPosition.left === $car.left) {
+//     console.log(racPosition.left, $car.left);
 
 //         isCollided = true;
 //         console.log(isCollided, "COLLIDED");
@@ -99,22 +110,19 @@ setInterval(() => {
 // }
 
 
+// create function to create an element, put it on page, and move it - from right side to left side of page 
 
 
-// // create function to create an element, put it on page, and move it - from right side to left side of page 
+// obstacle 12and3 and randomly generate
+// make sure obstacles don't generate on top of each other (timers?)
+// background position (background)
 
+// grab elements and check coordinates 
 
-// // obstacle 12and3 and randomly generate
-// // make sure obstacles don't generate on top of each other (timers?)
-// // background position (background)
+// get all coordinates functions, running all the time, checking item locations 
 
-// // grab elements and check coordinates 
-
-// // get all coordinates functions, running all the time, checking item locations 
-
-// // research 2d collision detection in jquery (mdm 2d collision)
-
-// // 
+// research 2d collision detection in jquery (mdm 2d collision)
+ 
 // var #raccoon = {x: ???, y: ???, width: 50, height: 50}
 // var #obstacle = {x: ???, y: ???, width: 90, height: 50}
 
@@ -143,12 +151,11 @@ setInterval(() => {
 // }
 
 
-// // code that gets coordinates dynamically as game runs 
-
+// code that gets coordinates dynamically as game runs 
 
 
 // CURRENT GOAL: randomize the spawm point of the car 
-// 300 - 0
+// 300 - 0?
 // Math.random 
 // $("#car").append("#car"(4([Math.floor = 300, Math.ceiling = 1 [Math.random])
 // ))
@@ -163,7 +170,7 @@ setInterval(() => {
 
 setInterval(() => {
     $(".car").eq(0).remove();
-    console.log("what the heck man");
+    console.log("testing to check random obstacle generation");
     $("<div>").addClass("car").css("top", Math.floor(Math.random() * 90) + "%").appendTo($(".container"));
     ;
 },3000)
